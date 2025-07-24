e9d85dca67155333ec4603cc804c92ec8292172f
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2025-07-24 14:46:38 +0500

    7. Function with *args
    Problem: Write a function that takes variable number of arguments and returns their sum.

---
+++ functions/07_solution.py
@@ -0,0 +1,24 @@
+def sum_all(*args):
+    return sum(args)
+
+print(sum_all(1, 2))
+print(sum_all(1, 2, 3, 4, 5))
+print(sum_all(1, 2, 3, 4, 5, 6, 7, 8))
+
+# def sum_all(*args):
+#     print(*args)
+#     return sum(args)
+
+# print(sum_all(1, 2))
+# print(sum_all(1, 2, 3, 4, 5))
+# print(sum_all(1, 2, 3, 4, 5, 6, 7, 8))
+
+# def sum_all(*args):
+#     print(args)
+#     for i in args:
+#         print(i * 2)
+#     return sum(args)
+
+# print(sum_all(1, 2, 3))
+# print(sum_all(1, 2, 3, 4, 5))
+# print(sum_all(1, 2, 3, 4, 5, 6, 7, 8))
