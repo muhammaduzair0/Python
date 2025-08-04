fe76abcfbdfb7044582136b8626fb2c7be56ce6c
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2025-08-04 14:22:07 +0500

    Add load_data() method to load YouTube videos from JSON file

--- error_handling/youtube_manager.py
+++ error_handling/youtube_manager.py
@@ -1,3 +1,11 @@
+
 import json
 
 youtube_file_txt = 'youtube.txt'
+
+def load_data():
+    try:
+        with open(youtube_file_txt, 'r') as file:
+          return json.load(file)
+    except FileNotFoundError:
+        return []
