845a5340863368d7b07b3ad477fb0f96ac09744f
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2025-08-04 14:24:18 +0500

    Add main loop with menu for YouTube Manager options

--- error_handling/youtube_manager.py
+++ error_handling/youtube_manager.py
@@ -28,3 +28,30 @@ def delete_video(videos):
     pass
 
 
+def main():
+    videos = load_data()
+    while True:
+        print("\n Youtube Manager | choose an option ")
+        print("1. List all youtube videos ")
+        print("2. Add a youtube video ")
+        print("3. Update a youtube video details ")
+        print("4. Delete a youtube video ")
+        print("5. Exit the app ")
+        choice = input("Enter your choice")
+
+        match choice:
+            case '1':
+                list_all_videos(videos)
+            case '2':
+                add_video(videos)
+            case '3':
+                update_video(videos)
+            case '4':
+                delete_video(videos)
+            case '5':
+                break
+            case _:
+                print("Invalid Choice")
+
+if __name__ == "__main__":
+    main()
