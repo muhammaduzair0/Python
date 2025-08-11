994f9c3630423112280bbbf4ed24f424542179d1
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2025-08-11 16:14:40 +0500

    Main Function Created With Conditions, Close DB

--- database_sqlite3/youtube_manager_db.py
+++ database_sqlite3/youtube_manager_db.py
@@ -29,3 +29,38 @@ def delete_video(video_id):
     cursor.execute("DELETE FROM videos WHERE id = ?", (video_id,))
     conn.commit()
 
+def main():
+    while True:
+        print("\n Youtube manager app with DB")
+        print("1. List Videos")
+        print("2. Add Videos")
+        print("3. Update Videos")
+        print("4. Delete Videos")
+        print("5. exit app")
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            list_videos()
+        elif choice == '2':
+            name = input("Enter the video name: ")
+            time = input("Enter the video time: ")
+            add_video(name, time)
+        elif choice == '3':
+            video_id = input("Enter video id to update: ")
+            name = input("Enter the video name: ")
+            time = input("Enter the video time: ")
+            update_video(video_id, name, time)
+        elif choice == '4':
+            video_id = input("Enter video ID to delete: ")
+            delete_video(video_id)
+        elif choice == '5':
+            break
+        else:
+            print("Invalid Choice ")
+    
+    conn.close()
+
+
+
+if __name__ == '__main__':
+    main()
