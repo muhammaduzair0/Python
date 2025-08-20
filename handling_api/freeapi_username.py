631b40140a075f07031ac5d662f6ca0148d8bdd4
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2025-08-20 20:24:22 +0500

    Fetch Random user api Function & Condition
     if Data has success key and data key then save data into user_data variable, go to user data key login and save username into username variable & goto user_data location key and save country data into country variable and return username, country

--- handling_api/freeapi_username.py
+++ handling_api/freeapi_username.py
@@ -1,2 +1,14 @@
 import requests
 
+def fetch_random_user_freeapi():
+    url = "https://api.freeapi.app/api/v1/public/randomusers/user/random"
+    response = requests.get(url)
+    data = response.json()
+
+    if data["success"] and "data" in data:
+        user_data = data["data"]
+        username = user_data["login"]["username"]
+        country = user_data["location"]["country"]
+        return username, country
+    else:
+        raise Exception("Failed to fetch user data")
